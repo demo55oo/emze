a8da3d5b00e78f5fa9dc77bb13e6921f2fde4f8c
Adham <adhammansour886@gmail.com>
2022-12-02 05:21:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Real-estate website using firebase : javascript
+
+### Demo Link: https://real-estate-cacfd.web.app
+
+To-DO:
+
+1. Add your firebase config on config.js file & on Login.html file.
+2. Setup your firebase authentication, realtime database & firebase storage.
---
+++ js/config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+//paste your config here
+apiKey: "AIzaSyDdwdZoPDEH7R4aESMyQC12Gbl5NgWZhiA",
+authDomain: "super-admin-panel-c8a78.firebaseapp.com",
+databaseURL: "https://super-admin-panel-c8a78-default-rtdb.firebaseio.com",
+projectId: "super-admin-panel-c8a78",
+storageBucket: "super-admin-panel-c8a78.appspot.com",
+messagingSenderId: "808974454858",
+appId: "1:808974454858:web:76ae7a1ab614f53800d6b3",
+measurementId: "G-WHJT914G2D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
